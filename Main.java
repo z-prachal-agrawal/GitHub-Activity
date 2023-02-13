5608ff8b6523e9cfcdd7d3a84d58d1c395b13bd4
Bhupender <bhupender@Bhupender-2.local>
2023-02-13 12:37:07 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        String s = "PRA12Ga782d45601$3";
+        int sum = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(Character.isDigit(s.charAt(i))){
+                sum = sum + Integer.parseInt(String.valueOf(s.charAt(i)));
+            }
+        }
+        System.out.print("Total Sum : " + sum);
+    }
+}
+//Output : Total Sum : 39
